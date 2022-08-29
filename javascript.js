4e05ff25ec7867b811e15b99db2958028c1c7617
jdfeng1 <justinfeng64@gmail.com>
2022-08-29 16:22:46 -0500

    Create gridRows variable

--- javascript.js
+++ javascript.js
@@ -1,1 +1,4 @@
 const gridContainer = document.querySelector('.grid-container');
+let root = document.querySelector(':root');
+let gridRows = getComputedStyle(root).getPropertyValue('--grid-rows');
+console.log(gridRows);
