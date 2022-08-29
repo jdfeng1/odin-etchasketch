0d54a7992924fb1a2f4c05c3186637ad779523b4
jdfeng1 <justinfeng64@gmail.com>
2022-08-29 16:49:23 -0500

    Create function to change row column

--- javascript.js
+++ javascript.js
@@ -1,4 +1,15 @@
 const gridContainer = document.querySelector('.grid-container');
 let root = document.querySelector(':root');
+let rootStyles = root.style;
 let gridRows = getComputedStyle(root).getPropertyValue('--grid-rows');
+let gridCols = getComputedStyle(root).getPropertyValue('--grid-columns');
+console.log(gridRows);
+
+let changeRowColumn = (row, col) => {
+    rootStyles.setProperty('--grid-rows', row);
+    rootStyles.setProperty('--grid-columns', col);
+
+}
+changeRowColumn(16, 16);
+gridRows = getComputedStyle(root).getPropertyValue('--grid-rows')
 console.log(gridRows);
