d98361237d5a4d622d54b5145139447fa7f85d2a
jdfeng1 <justinfeng64@gmail.com>
2022-08-29 18:52:20 -0500

    Add  code for drawing

--- javascript.js
+++ javascript.js
@@ -3,22 +3,41 @@ let root = document.querySelector(':root');
 let rootStyles = root.style;
 let gridRows = getComputedStyle(root).getPropertyValue('--grid-rows');
 let gridCols = getComputedStyle(root).getPropertyValue('--grid-columns');
+
+
+
 let slider = document.querySelector('#px-range');
 let slidePx = document.querySelectorAll('.px');
+let sliderVal = slider.value;
+
+rootStyles.setProperty('--grid-rows', sliderVal);
+rootStyles.setProperty('--grid-columns', sliderVal);
+for (let i = 0; i < sliderVal * sliderVal; i++) {
+    let cell = document.createElement("div");
+    gridContainer.appendChild(cell).className = `grid-item`;// grid${i}`;
+}
 for (let i = 0; i < slidePx.length; i++) {
-        slidePx[i].textContent = slider.value;
-    }
+    slidePx[i].textContent = slider.value;
+}
+
 
 let changeRowColumn = () => {
-let sliderVal = parseInt(slider.value)
+    sliderVal = slider.value;
     gridContainer.replaceChildren();
     rootStyles.setProperty('--grid-rows', sliderVal);
     rootStyles.setProperty('--grid-columns', sliderVal);
     for (let i = 0; i < sliderVal*sliderVal; i++) {
         let cell = document.createElement("div");
-        gridContainer.appendChild(cell).className = `grid-item grid${i}`;
+        gridContainer.appendChild(cell).className = `grid-item`;// grid${i}`;
     }
 }
+
+
+let changeColor = (e) => {
+    if(e.target.className = 'grid-item');
+    e.target.style.backgroundColor = 'black';
+}
+  
 
 let sliderDisplay = () => {
     for (let i = 0; i < slidePx.length; i++) {
@@ -26,6 +45,15 @@ let sliderDisplay = () => {
     }
 }
 
+let moveColor = () => {
+    gridContainer.addEventListener('mouseover', changeColor);
+}
+let stopColor = () => {
+    gridContainer.removeEventListener('mouseover', changeColor);
+}
+gridContainer.onmousedown = moveColor;
+gridContainer.onmouseup = stopColor;
 
 slider.onchange = changeRowColumn;
 slider.oninput = sliderDisplay;
+
