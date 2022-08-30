1888b8c68f93095fc72d7d00e461118c055ff7ff
jdfeng1 <justinfeng64@gmail.com>
2022-08-30 00:10:39 -0500

    Finish all main functionality

--- javascript.js
+++ javascript.js
@@ -14,6 +14,7 @@ let rainbowButton = document.querySelector('#rainbow-mode');
 let clearButton = document.querySelector("#clear");
 let colorBox = document.querySelector('.colorbox');
 
+let gridToggle = document.querySelector('.gridToggle');
 rootStyles.setProperty('--grid-rows', sliderVal);
 rootStyles.setProperty('--grid-columns', sliderVal);
 for (let i = 0; i < sliderVal * sliderVal; i++) {
@@ -56,6 +57,8 @@ let changeRowColumn = () => {
     }
 }
 
+
+
 colorBox.addEventListener('input', () => rootStyles.setProperty('--pen-color', colorBox.value));
 
 
@@ -64,7 +67,7 @@ let changeColor = (e) => {
 
     let penColor = colorBox.value;
     if (pencilButton.value == "ON") {
-        if(e.target.className = 'grid-item') {
+        if(e.target.classList.contains('grid-item')) {
             e.target.style.backgroundColor = penColor;
             e.target.style.opacity = 1;
         }
@@ -72,7 +75,7 @@ let changeColor = (e) => {
     }
 
     if (shaderButton.value == "ON") {
-        if(e.target.className = 'grid-item') {
+        if(e.target.classList.contains('grid-item')) {
             e.target.style.backgroundColor = penColor;
             if (e.target.style.opacity < 1) {
                 e.target.style.opacity -= -0.2;
@@ -83,7 +86,7 @@ let changeColor = (e) => {
 
     if (rainbowButton.value == "ON") {
         const randomColor = Math.floor(Math.random()*16777215).toString(16);
-        if(e.target.className = 'grid-item') {
+        if(e.target.classList.contains('grid-item')) {
             e.target.style.backgroundColor = "#" + randomColor;
             e.target.style.opacity = 1;
         }
@@ -108,4 +111,5 @@ gridContainer.onmouseup = stopColor;
 clearButton.addEventListener('click', changeRowColumn);
 slider.onchange = changeRowColumn;
 slider.oninput = sliderDisplay;
+
 
